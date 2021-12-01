e9f41ef8fee39a84fdb8053f7794131acdad7acf
Piotr Krol <krolpiotr225@gmail.com>
2021-12-01 13:21:27 +0100

    Add project files.

---
+++ ToDoList.Core/ViewModels/Pages/WorkTaskPagesViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using ToDoList.Core.ViewModels.Controls;
+
+namespace ToDoList.Core.ViewModels.Pages
+{
+    class WorkTaskPagesViewModel
+    {
+        private string netWorkTasktitle;
+
+        public List<WorkTaskViewModel> WotkTaskList { get; set; } = new List<WorkTaskViewModel>();
+        public string NetWorkTaskTitle { get; set; }
+        public string NetWotkTaskDesctipion { get; set; }
+        private void AddNewTask()
+        {
+            var newTask = new WorkTaskViewModel
+            {
+                Title = NetWorkTaskTitle,
+                Description = NetWotkTaskDesctipion,
+                CreatedDate = DateTime.Now
+            };
+            WotkTaskList.Add(newTask);
+
+        }
+    }
+}
